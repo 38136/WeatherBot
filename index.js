ef4435cccb4d79a12418d9667ff2c8f7225f2939
38136 <31004566+38136@users.noreply.github.com>
2017-08-21 13:47:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,58 @@
 // console.log("The follow bot is starting");
 
-const Twit = require("twit");
-const express = require("express");
-const bodyParser = require("body-parser");
-const apiai = require("apiai");
-const APIAII = apiai('8e19b5f4bcee4ca484320e31dfdfebf9');
-const fs = require("fs");
+console.log("The follow bot is starting");
+
+var Twit = require("twit");
+var express = require("express");
+var bodyParser = require("body-parser");
+var apiai = require("apiai");
+var APIAII = apiai('8e19b5f4bcee4ca484320e31dfdfebf9');
+var fs = require("fs");
 let weatherfunc = require('./weatherfunction');
-let  uploadMedia = require("./uploadpic");
+let uploadMedia = require("./uploadpic");
+ 
+const config = require("./config");
 
 let app = express();
+let T = new Twit(config);
+
+let stream = T.stream("user", {
+    stringify_friend_ids: true
+
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-const config = require("./config");
-
-let T = new Twit(config);
 // console.log(T);
 
-let stream = T.stream("user", {
-    stringify_friend_ids: true
-});
-
-stream.on("direct_message", (directMsg) => {
+stream.on("direct_message", function (directMsg) {
     let directMessage = directMsg.direct_message;
     let sender_id = directms.sender_id_str;
     let screenName = directMessage.sender.name;
-    let text = directMessage.text;
+    let txt = directMessage.text;
+    console.log(sender_id);
+    console.log(screen_name);
 
     fs.writeFileSync("./app.json", JSON.stringify(directMsg), "utf-8");
     if (text) {
         let request = APIAII.textRequest(text, {
             sessionId: 'SessionBot'
         });
-        request.on('response', (response) => {
+        request.on('response', function (response) {
             let responseQuery = response.result.resolvedQuery;
             let result = response;
             if (responseQuery == "hi") {
                 let image_media = JSON.parse(uploadMedia.TwitterUpload());
-               let welcomeMsg = weatherfunc.WelcomeParams(sender_id, screenName);
+                welcomeMsg = weatherfunc.WelcomeParams(sender_id, screenName, image_media.media_id_string);
+                console.log(sender_id);
+                console.log(screenName);
                 T.post("direct_messages/events/new", welcomeMsg, function (err, data, response) {
                     stream.stop();
                     stream.start();
                 });
-            } else
-                console.log("getting some issue");
+            }
             //  else if (responseQuery == "weather report") {
             //     welcomeMsg = weatherfunc.CategoryParams(sender_id, responseQuery);
             //     T.post("direct_messages/events/new", welcomeMsg, function (err, data, response) {
