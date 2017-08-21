ea0de8f8230179864bd6a71a716c070ec5d6b4d7
38136 <31004566+38136@users.noreply.github.com>
2017-08-21 12:02:22 +0530

    Add files via upload

---
+++ weatherfunction.js
@@ -0,0 +1,22 @@
+exports.WelcomeParams = function (sender_id, screen_name, media_id) {
+    return {
+        "event": {
+            "type": "message_create",
+            "message_create": {
+                "target": {
+                    "recipient_id": sender_id
+                },
+                "message_data": {
+                    "text": "Hi," + screen_name + "\n\nWelcome to weather channel !!!\n\n",
+                   
+                    "attachment": {
+                        "type": "media",
+                        "media": {
+                            "id": media_id
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
